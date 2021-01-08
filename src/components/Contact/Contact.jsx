facb6f0014408582e19c3b4ef42fa350ddff033f
zak <codedpills@gmail.com>
2021-01-08 14:41:59 +0000

    add skeleton for components

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Contact.css';
+
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                Contact
+            </div>
+        );
+    }
+}
+
+export default Contact;
