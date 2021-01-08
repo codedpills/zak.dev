05387ad910a7d7dbf7f5c531dfb687299d1851e9
zak <codedpills@gmail.com>
2021-01-08 14:58:13 +0000

    add routing

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import './Portfolio.css';
 
-import React, { Component } from 'react';
-
-class Portfolio extends Component {
-    render() {
-        return (
-            <div>
-                Portfolio
-            </div>
-        );
-    }
+const Portfolio = () => {
+    return (
+        <div>
+            Portfolio
+        </div>
+    );
 }
 
 export default Portfolio;
+
