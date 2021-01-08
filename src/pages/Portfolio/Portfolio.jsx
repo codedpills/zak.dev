b9796b085f73310bfaeb85031821bbaf66638362
zak <codedpills@gmail.com>
2021-01-08 14:45:08 +0000

    add skeleton for pages

---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Portfolio.css';
+
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+    render() {
+        return (
+            <div>
+                Portfolio
+            </div>
+        );
+    }
+}
+
+export default Portfolio;
