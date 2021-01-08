8e3ebddbcf2b4fb42fa2a4646686ad8b97cb1dda
zak <codedpills@gmail.com>
2021-01-08 22:17:00 +0000

    add services

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import "./Home.css";
 
 import Navbar from '../../components/Navbar/Navbar';
 import Hero from '../../components/Hero/Hero';
+import Services from '../../components/Services/Services';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Services />
     </div>
   );
 };
