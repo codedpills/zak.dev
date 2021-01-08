4d4f1c1d1f83034025b4a1cdc9bd5e5360ea5ff8
zak <codedpills@gmail.com>
2021-01-08 16:44:13 +0000

    add hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./Home.css";
 
 import Navbar from '../../components/Navbar/Navbar';
+import Hero from '../../components/Hero/Hero';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
