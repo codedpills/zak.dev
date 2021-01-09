9c810ffbc4609675b572143e9b3c965621ab3cb4
zak <codedpills@gmail.com>
2021-01-09 13:49:49 +0000

    collect contact info inside state

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,6 +2,25 @@ import React, { Component } from "react";
 import "./Contact.css";
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+    };
+  }
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    this.setState({ name: "", email: "", message: "" });
+  };
   render() {
     return (
       <div className="contact">
@@ -12,24 +31,38 @@ class Contact extends Component {
               <p className="text-center">
                 Have a question or want to work together?
               </p>
-              <br/>
-              <form>
+              <br />
+              <form onSubmit={this.handleSubmit}>
                 <div className="form-row">
                   <div className="form-group col-md-6">
-                    <label for="inputPassword4">Name</label>
-                    <input type="text" className="form-control" name="name" />
+                    <label htmlFor="inputPassword4">Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="name"
+                      value={this.state.name}
+                      onChange={this.handleChange}
+                    />
                   </div>
                   <div className="form-group col-md-6">
-                    <label for="inputEmail4">Email</label>
-                    <input type="email" className="form-control" name="email" />
+                    <label htmlFor="inputEmail4">Email</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                    />
                   </div>
                 </div>
                 <div className="form-group">
-                  <label for="inputAddress">Message</label>
+                  <label htmlFor="inputAddress">Message</label>
                   <textarea
                     class="form-control"
-                    name="message"
                     rows="3"
+                    name="message"
+                    value={this.state.message}
+                    onChange={this.handleChange}
                   ></textarea>
                 </div>
                 <button type="submit" className="btn btn-warning rounded-pill">
