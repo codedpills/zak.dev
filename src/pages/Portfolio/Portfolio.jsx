e8a1c85e4bd3fa71a9e8abd3a96882bc7a5755f8
zak <codedpills@gmail.com>
2021-01-09 18:08:02 +0000

    add portfolio

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Footer from '../../components/Footer/Footer';
+import Navbar from '../../components/Navbar/Navbar';
+import PortfolioContent from '../../components/Portfolio/Portfolio';
 import './Portfolio.css';
 
 const Portfolio = () => {
     return (
         <div>
-            Portfolio
+            <Navbar />
+            <PortfolioContent />
+            <Footer />
         </div>
     );
 }
